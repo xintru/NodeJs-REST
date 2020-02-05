875a464fcd48d124cf8c1aeb5ba0f71d2d375f1a
xintru <xintru@gmail.com>
2020-02-05 21:36:57 +0700

    added frontend and error handling

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
-
-const feedRoutes = require('./routes/feed')
-
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const path = require('path')
+const feedRoutes = require('./routes/feed')
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -17,4 +18,16 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes)
 
-app.listen(8080)
+app.use((error, req, res, next) => {
+  console.log(error)
+  const status = error.statusCode || 500
+  const message = error.message
+  res.status(status).json({ message: message })
+})
+
+mongoose
+  .connect(
+    'mongodb+srv://xintru:2p3c2635q@cluster0-rx2ra.mongodb.net/messages?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+  )
+  .then(res => app.listen(8080))
+  .catch(err => console.log(err))
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,19 +1,53 @@
+const { validationResult } = require('express-validator')
+const Post = require('../models/post')
+
 exports.getPosts = (req, res, next) => {
-  res
-    .status(200)
-    .json({ posts: [{ title: 'First post', content: 'This is first post!' }] })
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1',
+        title: 'First post',
+        content: 'This is first post!',
+        imageUrl: '/images/porech.jpg',
+        creator: {
+          name: 'Alex',
+        },
+        createdAt: new Date(),
+      },
+    ],
+  })
 }
 
 exports.postPost = (req, res, next) => {
+  const errors = validationResult(res)
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.')
+    error.statusCode = 422
+    throw error
+  }
   const title = req.body.title
   const content = req.body.content
-  // create db
-  res.status(201).json({
-    message: 'Post created!',
-    post: {
-      id: new Date().toISOString(),
-      title: title,
-      content: content,
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: '/images/porech.jpg',
+    creator: {
+      name: 'Alex',
     },
   })
+  post
+    .save()
+    .then(result => {
+      console.log(result)
+      res.status(201).json({
+        message: 'Post created!',
+        post: result,
+      })
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500
+      }
+      next(err)
+    })
 }
