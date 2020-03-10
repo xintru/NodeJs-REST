ffc3d67d986e30312a2420f82e517e91bc5f121f
Alexander Tolstokorov <xintru@gmail.com>
2020-03-10 18:22:58 +0700

    Delete secrets.js

--- secrets/secrets.js
+++
@@ -1,2 +0,0 @@
-exports.mongoDbKey =
-  'mongodb+srv://xintru:2p3c2635q@cluster0-rx2ra.mongodb.net/messages?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
